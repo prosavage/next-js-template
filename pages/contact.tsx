d9e9605d9d57e5c1d1671fd794a94a1401fae165
JasonWet <jason@colonialpress.net>
2021-01-18 17:43:14 -0500

    added SEO Head Tag

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,9 @@
+import Head from 'next/head';
 export default function Contact(props) {
+    <Head>
+      <title>Next-JS Template</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Next-JS Template Contact"></meta>
+    </Head>
     return <p>Contact</p>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import Head from 'next/head';
 import Button from '../components/ui/Button'
 import styled from "styled-components";
 export default function Home() {
+  <Head>
+      <title>Next-JS Template</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Next-JS Template Home"></meta>
+    </Head>
   return (
     <Wrapper>
       <h2>ProSavage's NextJS Template Application</h2>
