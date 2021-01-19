0cbda68dce55338d195a1449e5d87d4e386a0d61
JasonWet <jason@colonialpress.net>
2021-01-19 00:48:56 -0500

    changed head wording

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 export default function Contact(props) {
     <Head>
-      <title>Next-JS Template</title>
+      <title>Next.js Template</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Next-JS Template Contact"></meta>
+        <meta name="description" content="Contact Next.js Template"></meta>
     </Head>
     return <p>Contact</p>
 }
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 export default function FAQ(props) {
     <Head>
-      <title>Next-JS Template</title>
+      <title>Next.js Template</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Next-JS Template FAQ"></meta>
+        <meta name="description" content="FAQ Next.js Template"></meta>
     </Head>
     return <p>FAQ</p>
 }
